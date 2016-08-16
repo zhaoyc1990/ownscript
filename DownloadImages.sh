b60ec8102a0877a7200158a8e5de96c67fb84f2e
zhaoyc1990 <zhaoyc1990@163.com>
2016-08-16 14:23:03 +0800

    打错一个字

--- DownloadImages.sh
+++ DownloadImages.sh
@@ -4,7 +4,7 @@
 while true
 do
 	docker pull webdevops/php-nginx
-	# $? 用来判断上次bash 命令执行结果，成功退回0。，加“x”是为了保证不判断式参数不为空
+	# $? 用来判断上次bash 命令执行结果，成功返回0。，加“x”是为了保证不判断式参数不为空
 	if [ `echo $?`x = "0"x ]; then
 		exit 
 	fi
